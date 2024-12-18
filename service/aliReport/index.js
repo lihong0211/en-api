cdda0f00a59ceb44c7d0517f639e81425bc14dad
lihong <1419686503@qq.com>
2024-12-18 21:06:24 +0800

    feat: ali

--- service/aliReport/index.js
+++ service/aliReport/index.js
@@ -3,7 +3,6 @@ var mysql = require('mysql2/promise');
 var $conf = require('../../config/db.pdd.js');
 var $util = require('../../utils/index.js');
 var $sql = require('./sql.js');
-const { limit } = require('../../utils/index.js');
 //使用连接池
 var pool = mysql.createPool($util.extend({}, $conf.mysql));
 
@@ -23,32 +22,36 @@ module.exports = {
     } = req.body;
 
     pool.getConnection().then(async (connection) => {
-      connection
-        .query($sql.insert, [
-          createAt,
-          patientSex,
-          patientAge,
-          primaryDiagnosis,
-          illDesc,
-          sicknessDetail,
-          medicines,
-          pass,
-          reason,
-          query,
-        ])
-        .then(() => {
-          return res.json({
-            code: 200,
-            msg: 'success',
+      try {
+        connection
+          .query($sql.insert, [
+            createAt,
+            patientSex,
+            patientAge,
+            primaryDiagnosis,
+            illDesc,
+            sicknessDetail,
+            medicines,
+            pass,
+            reason,
+            query,
+          ])
+          .then(() => {
+            return res.json({
+              code: 200,
+              msg: 'success',
+            });
+          })
+          .catch((err) => {
+            return res.json({
+              code: 500,
+              msg: err.message,
+            });
           });
-        })
-        .catch((err) => {
-          return res.json({
-            code: 500,
-            msg: err.message,
-          });
-        });
-      connection.release();
+        connection.release();
+      } catch (e) {
+        console.log(e);
+      }
     });
   },
 };
