278d7b4f078864479aa4cdbf102023312f12eed9
lihong <1419686503@qq.com>
2024-12-18 21:15:11 +0800

    feat: sql

--- service/aliReport/sql.js
+++ service/aliReport/sql.js
@@ -1,11 +1,5 @@
 const sqlConfig = {
-  insert: `INSERT INTO ali_rp_check
-    (createAt, patientSex, patientAge, primaryDiagnosis, illDesc, sicknessDetail, medicines, pass, reason, query) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) 
-    ON DUPLICATE KEY UPDATE 
-    query = INSERT(query), 
-    pass = INSERT(pass), 
-    reason = INSERT(reason)`,
+  insert: `INSERT INTO ali_rp_check (createAt, patientSex, patientAge, primaryDiagnosis, illDesc, sicknessDetail, medicines, pass, reason, query) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE query = INSERT(query), pass = INSERT(pass), reason = INSERT(reason)`,
 };
 
 module.exports = sqlConfig;
